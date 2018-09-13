5d66f30a251b39954ea013d0ea4f430b6b708029
Yuqing Lu <luyuqing0708@gmail.com>
2018-09-13 19:43:27 +0200

    2018-09-13

--- mockBank/app.py
+++ mockBank/app.py
@@ -1,15 +1,41 @@
-from flask import Flask
+from flask import Flask, g, render_template, session, request, jsonify
 
-from database import get_db
+from database import update_auth, get_db
 
 
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    welcome_message = ''
+    if 'login_name' in session: 
+        name = session.get('login_name')
+        welcome_message = 'Welcome {}'.format(name)
+    else:
+        pass
+    return render_template('index.html', welcome_message=welcome_message)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form)
+        username = request.form.get('username')
+        password = request.form.get('password')
+        return render_template('balance.html')
+    else:
+        return render_template('login.html')
 
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
+    update_auth('foo', 0)
+    update_auth('peanut', 0)
+    db = get_db()
     if db is not None:
         db.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5005)
--- mockBank/database.py
+++ mockBank/database.py
@@ -7,14 +7,15 @@ db_dir = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(db_dir, 'database.db')
 
 
-# create a table for log-in user, not app related
+# create a table for log-in user
 def create_table():
     sql = """
     CREATE TABLE IF NOT EXISTS users (
         id integer PRIMARY KEY,
         name text NOT NULL,
         password text NOT NULL,
-        authenticated integer NOT NULL
+        authenticated integer NOT NULL,
+        balance integer NOT NULL
     );
     """
 
@@ -28,7 +29,7 @@ def create_table():
         conn.close()
 
 
-# shortcut to check if sqlite connection is closed, not app related
+# shortcut to check if sqlite connection is closed
 def check_conn_closed():
     conn = sqlite3.connect(db_path)
     conn.close()
@@ -39,15 +40,15 @@ def check_conn_closed():
         print(e)  # if conn closed, expected error: Cannot operate on a closed database.
 
 
-# add user to table users, not app related
-def add_user(id_, name, password, auth):
+# add user to table users
+def add_user(id_, name, password, auth, balance):
     sql = """
-    INSERT INTO users (id, name, password, authenticated) VALUES (?, ?, ?, ?)
+    INSERT INTO users (id, name, password, authenticated, balance) VALUES (?, ?, ?, ?, ?)
     """
     try:
         conn = sqlite3.connect(db_path)
         cursor = conn.cursor()
-        cursor.execute(sql, (id_, name, password, auth))
+        cursor.execute(sql, (id_, name, password, auth, balance))
         conn.commit()
     except Exception as e:
         print(e)
@@ -55,7 +56,7 @@ def add_user(id_, name, password, auth):
         conn.close()
 
 
-# select all users from users, not app related
+# select all users from users
 def select_user():
     sql = """SELECT * FROM users"""
     try:
@@ -69,8 +70,67 @@ def select_user():
         conn.close()
 
 
+# change auth status
+def update_auth(name, num):
+    sql = """update users set authenticated = ? where name = ?"""
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute(sql, (num, name))
+        conn.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+
+
+# check auth status
+def check_auth(name):
+    sql = """select authenticated from users where name = ?"""
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute(sql, (name,))
+        return cursor.fetchone()[0]
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+
+
+# check balance
+def check_balance(name):
+    sql = """select balance from users where name = ?"""
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute(sql, (name,))
+        return cursor.fetchone()[0]
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+
+
+# deduct balance
+def deduct_balance(name, amount):
+    balance = check_balance(name)
+    sql = """update users set balance = ? where name = ?"""
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        new_val = balance - amount
+        cursor.execute(sql, (new_val, name))
+        conn.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+
+
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(db_path)
     return db
+
